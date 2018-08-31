3b5a752cd4a00f5a10019b39abf69719547fff53
SanskritiL <32401372+SanskritiL@users.noreply.github.com>
2018-08-31 17:09:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function change(){
+    if(document.getElementById("button_2").value==='GET CODE !!!'){
+ document.getElementById("button_2").value = 'BRIDETOBE';
+    }
+    else{
+        document.getElementById("button_2").value = 'GET CODE !!!';   
+    }
+}
